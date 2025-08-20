b2bc623812a93755497ab8c248b7b4ea2f2ba615
Brian Haywood <bmhaywood1@gmail.com>
2025-08-20 18:02:11 -0400

    update

---
+++ index.js
@@ -0,0 +1,17 @@
+const PORT = 3000
+const axios = require('axios');
+const cheerio = require('cheerio');
+const express = require('express');
+
+const app = express()
+
+const url = 'https://www.theguardian.com/'
+
+axios(url)
+    .then(response => {
+        const html = response.data
+        console.log(html)
+    })
+
+
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
